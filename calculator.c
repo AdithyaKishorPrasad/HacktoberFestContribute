4d81b4e3ef850bfd649f1378d0623e778ec67f6d
Adithya kishor prasad <adithyaprasad86@gmail.com>
2019-10-31 23:56:38 +0530

    cal.c
    added extra operation

--- calculator.c
+++ calculator.c
@@ -5,7 +5,7 @@ int main(void)
   char operator;
     double n1,n2;
 
-    printf("Enter an operator (+, -, *,/): ");
+    printf("Enter an operator (+, -, *, / , %) ");
     scanf("%c", &operator);
 
     printf("Enter two numbers: ");
@@ -28,6 +28,9 @@ int main(void)
     case '/':
         printf("%lf / %lf = %lf ", n1, n2, n1 / n2);
         break;
+      
+    case '%':
+        printf("%lf % lf = %lf ",n1,n2,n1 % n2);
   }
   printf("\n");
   return 0;
